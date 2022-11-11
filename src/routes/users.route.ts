63efcf6360128978bb9bc9541ca7bc563059c814
Guilherme Santos <santosgui678@gmail.com>
2022-11-11 10:00:08 -0300

    Improve status codes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Router, Request, Response } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Gui' }];
 
-  res.status(200).send(users);
+  res.status(StatusCodes.OK).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  //get user by id in database
+  res.status(StatusCodes.OK).send({ uuid });
 });
 
 export default usersRoute;
