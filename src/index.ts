d3f5faf19ac1005511189a81df84e32f4352df9a
Guilherme Santos <santosgui678@gmail.com>
2022-11-11 10:19:05 -0300

    Refactor status route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
 import usersRoute from './routes/users.route';
 
 const app = express();
@@ -7,10 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'bar' });
-});
+app.use(statusRoute);
 
 app.listen(3000, () => {
   console.log('Ok!');
