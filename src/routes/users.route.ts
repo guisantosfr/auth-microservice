3ed39384f5d078cfcd4d233615eb24819540d2e4
Guilherme Santos <santosgui678@gmail.com>
2022-11-11 10:14:28 -0300

    Create put and delete endpoints

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -21,4 +21,14 @@ usersRoute.post('/users', (req: Request<{ uuid: string }>, res: Response, next: 
   res.status(StatusCodes.CREATED).send(newUser);
 });
 
+usersRoute.put('/users:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  const modifiedUser = req.body;
+  res.status(StatusCodes.OK).send(modifiedUser);
+})
+
+usersRoute.delete('/users:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  res.sendStatus(StatusCodes.OK);
+});
+
 export default usersRoute;
